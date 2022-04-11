7e25c2f16fd0e0b68582cbdd0c98528f7c72fad7
Md Adnan Hussain <mdadnanhusaain@gmail.com>
2022-04-11 17:17:55 +0530

    Renamed hello.py to add.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+import json
+import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = 'uealrsfidnvsdjcvnkcv'
 
 @app.route('/')
 def home():
@@ -9,6 +13,35 @@ def home():
 @app.route('/your-url', methods=['GET','POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+        
+        if request.form['code'] in urls.keys():
+            flash('Short Name Already taken. Please select another one.')
+            return redirect(url_for('home'))
+
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url' : request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('D:/Coding/Python/Flask/url-shortener/' + full_name)
+            urls[request.form['code']] = {'file' : full_name}
+        
+        with open('urls.json','w') as url_file:
+            json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
+
+@app.route('/<string:code>')
+def redirect_to_url(code): 
+    if os.path.exists('urls.json'):
+        with open ('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
